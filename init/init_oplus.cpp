8ca63dd8d4061448d5d19906e8be89849449be68
Alexander Koskovich <akoskovich@pm.me>
2022-05-12 01:06:04 -0700

    gunnar: init: Set model based on ro.boot.prjname
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: I06f9acda3fe77f57a1bc24ee5a802db0ab3b8d60

---
+++ init/init_oplus.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+using android::base::GetProperty;
+
+/*
+ * SetProperty does not allow updating read only properties and as a result
+ * does not work for our use case. Write "OverrideProperty" to do practically
+ * the same thing as "SetProperty" without this restriction.
+ */
+void OverrideProperty(const char* name, const char* value) {
+    size_t valuelen = strlen(value);
+
+    prop_info* pi = (prop_info*)__system_property_find(name);
+    if (pi != nullptr) {
+        __system_property_update(pi, value, valuelen);
+    } else {
+        __system_property_add(name, strlen(name), value, valuelen);
+    }
+}
+
+/*
+ * Only for read-only properties. Properties that can be wrote to more
+ * than once should be set in a typical init script (e.g. init.oplus.hw.rc)
+ * after the original property has been set.
+ */
+void vendor_load_properties() {
+    //auto device = GetProperty("ro.product.product.device", "");
+    auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
+
+    if (prjname == 20825) // Unlocked
+        OverrideProperty("ro.product.product.model", "CPH2459");
+    else { // T-Mobile or Metro (20826)
+        OverrideProperty("ro.product.product.model", "GN2200");
+        OverrideProperty("ro.product.product.device", "OP515AL1");
+        OverrideProperty("ro.product.product.name", "GN2200");
+    }
+}
