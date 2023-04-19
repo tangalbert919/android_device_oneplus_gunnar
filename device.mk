f5e1a7394b250e8d9b0935fe361bc5cbeebf1dbb
Albert Tang <tangalbert919@yahoo.com>
2023-04-19 11:42:37 -0500

    gunnar: Inherit 6 GB dalvik VM config
    Change-Id: Ic2a432340909db4cc69101728b01d36dcaf12373

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/gunnar/gunnar-vendor.mk)
